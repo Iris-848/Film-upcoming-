d2ceff5d2e65048f97a16533a1584e6f7b3d9c8d
Iris <chrysoliontarispican3@gmail.com>
2026-01-27 12:39:15 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,15 +3,30 @@ const minTime = 2500;
 
 window.addEventListener("load", () => {
 
-  const loading = document.querySelector(".loading")
+  const loading = document.querySelector(".loading");
   const elapsedTime = Date.now() - startTime;
 
   setTimeout(() => {
-    loading.classList.add("loading_fadeout");
+    loading.classList.add("fadeout");
 
     setTimeout(() => {
       loading.style.display = "none";
     }, 500);
   }, minTime - elapsedTime);
 
+});
+
+const modalTrailerClose = document.querySelector(".modal_trailer_close")
+
+modalTrailerClose.addEventListener("click", () => {
+
+  const modalTrailer = document.querySelector(".modal_trailer");
+
+  setTimeout(() => {
+    modalTrailer.classList.add("fadeout");
+
+    setTimeout(() => {
+      modalTrailer.style.display = "none";
+    }, 500);
+  });
 });
